2585a94093bf02929acb5fd87fd1cb56c38710da
Julio Espinoza <a111@Julio-MacPro.local>
2026-06-22 12:18:38 -0600

    feat: add product pages templates with custom placeholders and responsive layout

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        mallaRaschel: resolve(__dirname, 'malla-raschel.html'),
+        mallaMonofilamento: resolve(__dirname, 'malla-monofilamento.html'),
+        lonaArquitectonica: resolve(__dirname, 'lona-arquitectonica.html'),
+        lonaToldos: resolve(__dirname, 'lona-toldos.html'),
+        lonaTransportistas: resolve(__dirname, 'lona-transportistas.html')
+      }
+    }
+  }
+});
