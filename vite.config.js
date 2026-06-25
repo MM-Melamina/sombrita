5d50180527dad97e278a6c16851176baca41ee42
Julio Espinoza <a111@Julio-MacPro.local>
2026-06-25 10:21:37 -0600

    fix: include galeria.html as input in vite.config.js to compile it on build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         mallaRaschel: resolve(__dirname, 'malla-raschel.html'),
         lonas: resolve(__dirname, 'lonas.html'),
-        plasticoNegro: resolve(__dirname, 'plastico-negro.html')
+        plasticoNegro: resolve(__dirname, 'plastico-negro.html'),
+        galeria: resolve(__dirname, 'galeria.html')
       }
     }
   }
